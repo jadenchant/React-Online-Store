9df5afcec7a91f90333437342f182cadc76b727b
Jaden Chant <chant1@stolaf.edu>
2021-01-26 00:11:11 -0600

    small changes to the view of the section css layout

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -4,12 +4,6 @@ import PushDownContent from './PushDownContent';
 import '../css/content.css';
 const Content=() => {
 
-    const data=(
-        <div className="box">
-            <h2>Div Content</h2>
-        </div>
-    );
-
     return (
         <main className="content">
             <PushDownContent />
