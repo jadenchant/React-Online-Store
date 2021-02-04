5a60858b7769d38aacdc9f3905822016d64ba6aa
Jaden Chant <chant1@stolaf.edu>
2021-02-04 17:09:29 -0600

    commented out the non working lines

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Section from './Section2';
 import PushDownContent from './PushDownContent';
 import '../css/content.css';
 const Content=() => {
+    // const [isDark, setIsDark] = useState('light');
+
+    // const toggleMode = () => {
+    //     isDark ? setIsDark('dark') : setIsDark('light');
+    // }
+
     return (
         <main className="content">
             <PushDownContent />
             <Section name="suggested" />
             <Section name="popular" />
+            {/* <button onClick={toggleMode()}>{isDark === 'dark' ? 'Light' : 'Dark'}</button> */}
         </main>
     );
 }
